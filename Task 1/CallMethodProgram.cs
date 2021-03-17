05ba62ec83f6a4e27c0c20a08ee1fb1cdfe59f14
Drazenko Sikimic <dsikimic2@gmail.com>
2021-03-17 15:22:49 +0100

    Task 1

---
+++ Task 1/CallMethodProgram.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class CallMethodProgram
+    {
+
+        public void Test()
+        {
+            Console.WriteLine("Hello from test method");
+        }
+
+        static void Main(string[] args)
+        {
+            string methodeName = null;
+            while (methodeName == null || methodeName.Length == 0)
+            {
+                Console.Write("Plase insert methode name: ");
+                methodeName = Console.ReadLine();
+            }
+
+            CallMethodProgram callMethodeProgram = new CallMethodProgram();
+
+            MethodInfo method = null;
+            try
+            {
+                method = callMethodeProgram.GetType().GetMethod(methodeName);
+            }
+            catch (AmbiguousMatchException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            if (method == null)
+            {
+                Console.WriteLine("Method doesn't exists");
+            }
+            else
+            {
+                method.Invoke(callMethodeProgram, null);
+            }
+            Console.WriteLine("Press any key for exit");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task 1/Exceptions/AlreadyStartedException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Task_1.Utils
+{
+    [Serializable]
+    internal class AlreadyStartedException : Exception
+    {
+        public AlreadyStartedException()
+        {
+        }
+
+        public AlreadyStartedException(string message) : base(message)
+        {
+        }
+
+        public AlreadyStartedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected AlreadyStartedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
